959bb7eeb184d9db9c16c6f95a94b8423acb4487
Ben Hollis <ben@benhollis.net>
2017-06-24 15:48:31 -0700

    Fudge keys around

--- release.sh
+++ release.sh
@@ -4,12 +4,14 @@ git config --global user.email "destinyitemmanager@gmail.com"
 git config --global user.name "DIM Release Bot"
 
 # Decrypt SSH key
-openssl aes-256-cbc -K $encrypted_3184b4fb5b91_key -iv $encrypted_3184b4fb5b91_iv -in id_rsa.enc -out DIM/config/dim_travis.rsa -d
-chmod 600 DIM/config/dim_travis.rsa
-echo -e "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile $PWD/DIM/config/dim_travis.rsa\n" >> ~/.ssh/config
+openssl aes-256-cbc -K $encrypted_3184b4fb5b91_key -iv $encrypted_3184b4fb5b91_iv -in id_rsa.enc -out ~/.ssh/dim_travis.rsa -d
+chmod 600 ~/.ssh/dim_travis.rsa
+echo -e "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/dim_travis.rsa\n" >> ~/.ssh/config
 
 # Clone project
 git clone git@github.com:DestinyItemManager/DIM.git -b dev --depth 1
+
+cp ~/.ssh/dim_travis DIM/config
 
 cd DIM
 
